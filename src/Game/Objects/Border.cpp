d22b8c64462b8348503dfdf730e4ec38dd4cf61e
LiWinDom <wadim.gorbachev@mail.ru>
2024-11-10 11:39:19 +0300

    Added borders to game field

---
+++ src/Game/Objects/Border.cpp
@@ -0,0 +1,13 @@
+#include "Border.h"
+
+const sf::Vector2f positions[] = { sf::Vector2f(0, -208), sf::Vector2f(208, 0), sf::Vector2f(0, 208), sf::Vector2f(-208, 0) };
+
+Border::Border(uint8_t borderNum) : IObject(ObjectType::Border, positions[borderNum], sf::Vector2f(208, 208)) {}
+
+uint8_t Border::getState() const {
+  return 0;
+}
+
+void Border::think(const std::vector<std::shared_ptr<IObject>> &objects, uint16_t index, const sf::Clock &globalClock, const Event &event) {
+  return;
+}
