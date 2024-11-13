256b6b61239b638eebbc6144582360f9f0221d34
LiWinDom <wadim.gorbachev@mail.ru>
2024-11-13 11:57:03 +0300

    Added stats background

---
+++ src/Game/Render/Drawables/BorderDrawable.cpp
@@ -0,0 +1,16 @@
+#include "BorderDrawable.h"
+
+#include "../../../Other/Log.h"
+
+BorderDrawable::BorderDrawable() {
+  if (!_texture.loadFromFile(Path::getAbsolutePath("resources/graphics/StatsBackground.png"))) {
+    throw std::runtime_error(R"(Cannot load "resources/graphics/StatsBackground.png" texture)");
+  }
+  _sprites.emplace_back(_texture);
+  Log::debug(R"(Drawable "Border" created)");
+}
+
+void BorderDrawable::update(const std::shared_ptr<IObject> &object) {
+  _sprites[0].setTextureRect(sf::IntRect(0, 0, 32, 208));
+  _sprites[0].setPosition(object->getPosition());
+}
