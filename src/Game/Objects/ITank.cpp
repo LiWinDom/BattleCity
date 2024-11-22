594f3c7bee708a6ce0d723f9951a1b94289af809
LiWinDom <wadim.gorbachev@mail.ru>
2024-11-22 11:53:32 +0300

    Realized layer collision system

--- src/Game/Objects/ITank.cpp
+++ src/Game/Objects/ITank.cpp
@@ -1,6 +1,8 @@
 #include "ITank.h"
 
-ITank::ITank(ObjectType type, const sf::Vector2f& position) : IMovable(type, position, sf::Vector2f(16, 16), 45) {}
+ITank::ITank(ObjectType type, const sf::Vector2f& position) : IMovable(type, position, sf::Vector2f(16, 16), 45) {
+  _collisionLayer = 1;
+}
 
 uint8_t ITank::getState() const {
   return _wheelState << 2 | (uint8_t)_rotation;
