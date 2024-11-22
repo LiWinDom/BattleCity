c02930cff7d43385b565c7ce5910851f363d9846
LiWinDom <wadim.gorbachev@mail.ru>
2024-11-22 14:47:58 +0300

    Added bullets limit to tanks

--- src/Game/Objects/ITank.h
+++ src/Game/Objects/ITank.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "Bullet.h"
 #include "IMovable.h"
 
 class ITank : public IMovable {
@@ -8,8 +9,14 @@ class ITank : public IMovable {
 
   uint8_t getState() const override;
 
+  void think(std::vector<std::shared_ptr<IObject>> &objects, const sf::Clock& globalClock, const Event& event) override;
+
+  void shoot(std::vector<std::shared_ptr<IObject>> &objects);
+
  protected:
   static void snap(float& coordinate);
 
   bool _wheelState = false;
+  uint8_t _maxBullets = 1;
+  std::vector<std::shared_ptr<IObject>> _bullets;
 };
--- src/Game/Objects/PlayerTank.cpp
+++ src/Game/Objects/PlayerTank.cpp
@@ -5,43 +5,6 @@
 PlayerTank::PlayerTank(const sf::Vector2f& position) : ITank(ObjectType::PlayerTank, position) {}
 
 void PlayerTank::think(std::vector<std::shared_ptr<IObject>> &objects, const sf::Clock& globalClock, const Event& event) {
-  // Movement
-  if (event.up || event.down || event.left || event.right) {
-    auto oldPosition = _position;
-
-    // Rotating and snapping
-    if (event.up) {
-      snap(_position.x);
-      snap(oldPosition.x);
-      _rotation = ObjectRotation::Up;
-    } else if (event.left) {
-      snap(_position.y);
-      snap(oldPosition.y);
-      _rotation = ObjectRotation::Left;
-    } else if (event.down) {
-      snap(_position.x);
-      snap(oldPosition.x);
-      _rotation = ObjectRotation::Down;
-    } else {
-      snap(_position.y);
-      snap(oldPosition.y);
-      _rotation = ObjectRotation::Right;
-    }
-
-    const auto movedTiles = move(globalClock);
-    if (movedTiles % 2) {
-      _wheelState = !_wheelState;
-    }
-    if (!getHardCollisions(objects).empty()) {
-      _position = oldPosition;
-    }
-  }
-  else {
-    move(globalClock, false);
-  }
-
-  // Shooting
-  if (event.shoot) {
-    objects.push_back(std::make_shared<Bullet>(_position, _rotation));
-  }
+  // Base think
+  ITank::think(objects, globalClock, event);
 }
