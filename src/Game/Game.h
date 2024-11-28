bcb1da2cbacce22f0adcd87340a8ace027b54189
LiWinDom <wadim.gorbachev@mail.ru>
2024-11-28 18:31:01 +0300

    Game now accepts string of game field

--- src/Game/Game.h
+++ src/Game/Game.h
@@ -6,16 +6,17 @@
 #include "Event.h"
 #include "Objects/IObject.h"
 
-class GameField {
+class Game {
  public:
-  GameField(const std::vector<std::shared_ptr<IObject>>& objects, const std::vector<uint8_t>& tanks, const bool homebrewChanges = true);
-  GameField(const std::vector<std::string>& objects, const std::string& tanks, const bool homebrewChanges = true);
+  Game(const std::vector<std::shared_ptr<IObject>>& objects, const std::vector<uint8_t>& tanks, const bool homebrewChanges = true);
+  Game(const std::vector<std::string>& objects, const std::string& tanks, const bool homebrewChanges = true);
 
   std::vector<std::shared_ptr<IObject>> getObjects() const;
   void think(const Event& event);
 
  private:
   std::vector<std::shared_ptr<IObject>> _objects;
+  std::vector<uint8_t> _tanks;
   bool _homebrew;
 
   sf::Clock _globalClock;
