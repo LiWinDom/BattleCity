4193257cc637bec5fc1b1f784c7075aca09c8108
LiWinDom <wadim.gorbachev@mail.ru>
2024-12-03 17:52:20 +0300

    Border fix

--- src/Game/Objects/Border.h
+++ src/Game/Objects/Border.h
@@ -5,8 +5,4 @@
 class Border : public IObject {
  public:
   Border(uint8_t borderNum);
-
-  uint8_t getState() const override;
-
-  void think(std::vector<std::shared_ptr<IObject>> &objects, const sf::Clock &globalClock, const Event &event) override;
 };
