fb7bba6a3e4d8a0965ec65e3c4f84a47f4736100
LiWinDom <wadim.gorbachev@mail.ru>
2024-12-03 17:52:20 +0300

    Border fix

--- src/Game/Objects/Border.cpp
+++ src/Game/Objects/Border.cpp
@@ -3,7 +3,3 @@
 const sf::Vector2f positions[] = { sf::Vector2f(0, -208), sf::Vector2f(208, 0), sf::Vector2f(0, 208), sf::Vector2f(-208, 0) };
 
 Border::Border(uint8_t borderNum) : IObject(ObjectType::Border, positions[borderNum], sf::Vector2f(208, 208)) {}
-
-uint8_t Border::getState() const {
-  return 0;
-}
